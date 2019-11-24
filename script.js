b4d1942cb9a3a19906716596d599ae35b2ae7cc9
Di Wen <ifyouseewendy@gmail.com>
2019-11-24 14:13:46 -0500

    Try frappe gantt chart

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-google.charts.load('current', { 'packages': ['gantt'] });
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-  var options = {
-    height: 1000,
-    gantt: {
-        trackHeight: 30
-    }
-  };
-
-  var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
-
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Task ID');
-  data.addColumn('string', 'Task Name');
-  data.addColumn('string', 'Issue');
-  data.addColumn('date', 'Start Date');
-  data.addColumn('date', 'End Date');
-  data.addColumn('number', 'Duration');
-  data.addColumn('number', 'Percent Complete');
-  data.addColumn('string', 'Dependencies');
-
-  fetch("https://raw.githubusercontent.com/ifyouseewendy/verbose-disco/master/issues.json")
-    .then(response => response.json())
-    .then(function(issues) {
-        issues.forEach(
-          issue => data.addRow([
-              issue.title,
-              issue.title,
-              issue.url,
-              new Date(issue.start_date[0], issue.start_date[1] - 1, issue.start_date[2]),
-              new Date(issue.end_date[0], issue.end_date[1] - 1, 30),
-              null,
-              100,
-              null
-          ])
-        )
-        chart.draw(data, options);
-    })
-}
+fetch("https://raw.githubusercontent.com/ifyouseewendy/verbose-disco/8211128fc38bfadc158c8b336167b30289c6c5f8/issues.json")
+  .then(response => response.json())
+  .then(function(issues) {
+    var tasks = [];
+    issues.forEach(function(issue) {
+      tasks.push({
+        id: issue.title,
+        name: issue.title,
+        start: issue.start_date.join("-"),
+        end: issue.end_date.join("-"),
+        progress: 100,
+        dependencies: null,
+        url: issue.url
+      });
+    });
+    var gantt = new Gantt("#gantt", tasks, {
+      custom_popup_html: function(task) {
+        // the task object will contain the updated
+        // dates and progress value
+        return `
+          <div class="title">${task.name}</div>
+          <div class="subtitle">
+            <p>${task.start} - ${task.end}</p>
+            <p><a href="${task.url}" target="_blank">Detail</a></p>
+          </div>
+        `;
+      }
+    });
+    gantt.change_view_mode('Month');
+  })
